27fdc7726e9f5fed19b0861b911ec52cec482f99
ellijayne <ellijayne.m.willard@gmail.com>
2019-12-01 14:27:01 +1100

    added slick slider default

---
+++ js/main.js
@@ -0,0 +1,10 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+
+    $('.hero-carousel').slick({ 
+
+        autoplay: true,
+        autoplaySpeed: 3000,
+        arrows: true
+    });
+
+});
